4f23c0e916fae406b0e970f3bebf22f5e21987a0
Claudinei Perboni <cperbony@gmail.com>
2021-07-18 18:59:29 -0300

    refactor main for factory

--- main.rb
+++ main.rb
@@ -1,16 +1,27 @@
 require_relative './lib/board'
+require_relative './lib/board_rendered_text'
 require_relative './lib/pawn'
 require_relative './lib/rook'
+require_relative './lib/bishop'
+require_relative './lib/king'
+require_relative './lib/queen'
+require_relative './lib/knight'
 
-board = Board.new
-board[[1, 0]] = Pawn.new([0, 0], :black)
-board[[1, 1]] = Pawn.new([0, 0], :black)
-board[[1, 2]] = Pawn.new([0, 0], :black)
-board[[1, 3]] = Pawn.new([0, 0], :white)
+# Using Factory
+board = Board.start_chess
 
-puts board.grid
+text_board = BoardRenderedText.new(board)
+puts text_board.render
 
-puts board.in_bounds?([-1, 0])
+# board = Board.new
+# board[[1, 0]] = Pawn.new([0, 0], :black)
+# board[[1, 1]] = Pawn.new([0, 0], :black)
+# board[[1, 2]] = Pawn.new([0, 0], :black)
+# board[[1, 3]] = Pawn.new([0, 0], :white)
 
-pawn = Pawn.new([0, 0], :black)
-puts pawn.to_s
+# puts board.grid
+
+# puts board.in_bounds?([-1, 0])
+
+# pawn = Pawn.new([0, 0])
+# puts pawn.to_s
