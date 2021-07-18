6ffdb1dc37b69b606838d8aea0f2dd4da14e9e8c
Claudinei Perboni <cperbony@gmail.com>
2021-07-18 18:59:14 -0300

    Create a render for board

---
+++ lib/board_rendered_text.rb
@@ -0,0 +1,23 @@
+class BoardRenderedText
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def render
+    8.times do |r|
+      puts '----------------------'
+      8.times do |c|
+        piece = board[[r, c]] 
+        if piece.nil?
+          print ' '
+        else
+          print "#{ piece }  "
+        end
+      end
+      puts ''
+    end
+    puts '----------------------'
+  end
+end
